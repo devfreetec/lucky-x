71e6ec7a14f86b709c9959c8aadf4a007f59d665
devfreetec <devfreetech@yahoo.com>
2024-11-29 21:00:41 +0300

    Update ans.js

--- commandes/ans.js
+++ commandes/ans.js
@@ -45,7 +45,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
 
 
 
-  zokou({ nomCom: "bing", reaction: "🌀", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+  zokou({ nomCom: "bing4", reaction: "🌀", categorie: "IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
   
     try {
@@ -73,7 +73,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
     }
   });
   
-  zokou({ nomCom: "ai", reaction: "🌀", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+  zokou({ nomCom: "ai2", reaction: "🌀", categorie: "IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
   
     try {
@@ -98,7 +98,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   });
 
 
-zokou({ nomCom: "gpt", reaction: "🌀", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "gpt-all", reaction: "🌀", categorie: "IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
   
     try {
